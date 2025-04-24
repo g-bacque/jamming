14ac4809ec666f2983dd5133a0a178d4c8ea5b88
g-bacque <gbbgithub@gmail.com>
2025-04-24 11:11:47 +0200

    new static components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar.js'
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <SearchBar />
+
+        <p> This is my first change to the React App.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/SearchBar.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function SearchBar(){
+    return (
+        <h1>This is a search bar</h1>
+    )
+
+}
+
+export default SearchBar
